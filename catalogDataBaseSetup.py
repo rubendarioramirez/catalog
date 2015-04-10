fd48454398c2e65ecce6be0b3570b8e878fb1dc8
rubendarioramirez <ramirez.ruben.dario10@gmail.com>
2015-04-10 22:23:10 +0700

    Added G+

--- catalogDataBaseSetup.py
+++ catalogDataBaseSetup.py
@@ -8,39 +8,24 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
  
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+class Category(Base):
+    __tablename__ = 'category'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
  
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
-
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
 
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant) 
+    category_id = Column(Integer,ForeignKey('category.id'))
+    category = relationship(Category) 
 
-    #We added this serialize function to be able to send JSON objects in a serializable format
-    @property
-    def serialize(self):       
-        return {
-        'name': self.name,
-        'description':  self.description,
-        'id':   self.id,
-        'price':    self.price,
-        'course':   self.course
-        }
- 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
+engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.create_all(engine)
 
